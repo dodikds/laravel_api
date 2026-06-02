4b59c0e99c91dc1d3ac3793644919ed965c62410
Dodik Dwi Sancoko <dodikds@gmail.com>
2026-06-02 23:19:03 +0700

    Delete a resource (DELETE Endpoint)

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -58,6 +58,8 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->noContent();
     }
 }
